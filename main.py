c16da1318e3b341051b38e93cb444369d601037f
aghabidareh <aghabidareh@gmail.com>
2025-05-25 10:20:21 +0330

    normal-dashboard-loading-data

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import pandas as pd
 import plotly.express as px
 from dash import Dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
+
+# Load Data
+df = pd.read_csv('netflix_titles.csv')
