20bb4012dfa236baaf3292d2e2b9278d6f2cdf54
aghabidareh <aghabidareh@gmail.com>
2025-05-25 10:24:41 +0330

    main-dashboard-initializer

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import dash
 from dash import dcc, html, Input, Output
 import pandas as pd
 import plotly.express as px
+
+df = pd.read_csv("netflix_titles.csv")
+df['date_added'] = pd.to_datetime(df['date_added'])
+df['release_year'] = df['release_year'].fillna(0).astype(int)
+df['country'] = df['country'].fillna("Unknown")
+df['listed_in'] = df['listed_in'].fillna("Unknown")
