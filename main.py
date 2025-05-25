e7afbd1ba88d41ea41777c3b0df096772affb1e0
aghabidareh <aghabidareh@gmail.com>
2025-05-25 10:26:59 +0330

    debug

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv("netflix_titles.csv")
-df['date_added'] = pd.to_datetime(df['date_added'])
+df['date_added'] = pd.to_datetime(df['date_added'].str.strip(), errors='coerce')
 df['release_year'] = df['release_year'].fillna(0).astype(int)
 df['country'] = df['country'].fillna("Unknown")
 df['listed_in'] = df['listed_in'].fillna("Unknown")
