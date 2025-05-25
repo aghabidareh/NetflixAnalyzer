0d945c5badafe5ebf72169f9de536e757850043d
aghabidareh <aghabidareh@gmail.com>
2025-05-25 10:24:55 +0330

    main-dashboard-initializer

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ df['date_added'] = pd.to_datetime(df['date_added'])
 df['release_year'] = df['release_year'].fillna(0).astype(int)
 df['country'] = df['country'].fillna("Unknown")
 df['listed_in'] = df['listed_in'].fillna("Unknown")
+
+years = sorted(df['release_year'].unique())
+countries = sorted(set(', '.join(df['country'].dropna()).split(', ')))
+types = df['type'].unique()
